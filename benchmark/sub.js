999807608956dabdd958d723f069f4b9d34f21a9
TJ Holowaychuk <tj@vision-media.ca>
2012-07-12 07:16:55 -0700

    add mean and median to benchmark

--- benchmark/sub.js
+++ benchmark/sub.js
@@ -8,12 +8,38 @@ sock.connect(3000);
 var n = 0;
 var ops = 200;
 var t = process.hrtime();
+var results = [];
 
 sock.on('message', function(msg){
   if (n++ % ops == 0) {
     t = process.hrtime(t);
     var ms = t[1] / 1000 / 1000;
-    process.stdout.write('\r  [' + (ops * (1000 / ms) | 0) + ' ops/s] [' + n + ']');
+    var persec = (ops * (1000 / ms) | 0);
+    results.push(persec);
+    process.stdout.write('\r  [' + persec + ' ops/s] [' + n + ']');
     t = process.hrtime();
   }
+});
+
+function sum(arr) {
+  return arr.reduce(function(sum, n){
+    return sum + n;
+  });
+}
+
+function mean(arr) {
+  return sum(arr) / arr.length | 0;
+}
+
+function median(arr) {
+  arr = arr.sort();
+  return arr[arr.length / 2 | 0];
+}
+
+process.on('SIGINT', function(){
+  console.log('\n');
+  console.log('     mean: %d', mean(results));
+  console.log('   median: %d', median(results));
+  console.log();
+  process.exit();
 });
