742cb9cb79179480f7b67141aa57e7166e338c6e
TJ Holowaychuk <tj@vision-media.ca>
2012-07-12 07:06:37 -0700

    add new benchmarks

--- benchmark/sub.js
+++ benchmark/sub.js
@@ -6,6 +6,15 @@ var ss = require('..')
 sock.connect(3000);
 console.log('sub connected');
 
+var n = 0;
+var ops = 200;
+var t = process.hrtime();
+
 sock.on('message', function(msg){
-  console.log(msg.toString());
+  if (n++ % ops == 0) {
+    t = process.hrtime(t);
+    var ms = t[1] / 1000 / 1000;
+    process.stdout.write('\r  [' + (ops * (1000 / ms) | 0) + ' ops/s] [' + n + ']');
+    t = process.hrtime();
+  }
 });
