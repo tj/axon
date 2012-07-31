dca594fb7bb56a526637302fd42addfae444dd35
Garrett Johnson <gjohnson@redventures.com>
2012-07-31 05:26:11 -0400

    Add new multipart tests

--- test/test.parser.js
+++ test/test.parser.js
@@ -1,27 +1,26 @@
 
 var ss = require('..')
-  , parser = new ss.Parser
+  , decoder = new ss.Decoder
   , should = require('should')
-  , msgs;
+  , msgs
+  , decoder;
 
 // capture messages
 
-parser._onmessage = function(msg, meta){
-  msgs.push({ payload: msg, meta: meta });
+decoder.onmessage = function(body, multipart){
+  msgs.push({ body: body, multipart: multipart });
 };
 
 // test well formed packets
 
 msgs = [];
 
-parser.write(new Buffer([0x00, 0x00, 0x00, 0x01, 0x30]));
-parser.write(new Buffer([0x00, 0x00, 0x00, 0x01, 0x30]));
-parser.write(new Buffer([0x00, 0x00, 0x00, 0x01, 0x30]));
+decoder.write(new Buffer([0x01, 0x00, 0x00, 0x01, 0x30]));
+decoder.write(new Buffer([0x01, 0x00, 0x00, 0x01, 0x30]));
+decoder.write(new Buffer([0x01, 0x00, 0x00, 0x01, 0x30]));
 
 for (var i = 0; i < 3; ++i) {
-  msgs[i].payload.toString().should.equal('0');
-  msgs[i].meta.length.should.equal(1);
-  msgs[i].meta.format.should.equal(0);
+  msgs[i].body.toString().should.equal('0');
 }
 
 // test single bytes
@@ -29,39 +28,62 @@ for (var i = 0; i < 3; ++i) {
 msgs = [];
 
 var bytes = [
-  0x00, 0x00, 0x00, 0x01, 0x32,
-  0x00, 0x00, 0x00, 0x01, 0x32,
-  0x00, 0x00, 0x00, 0x01, 0x32,
-  0x00, 0x00, 0x00, 0x01, 0x32,
-  0x00, 0x00, 0x00, 0x01, 0x32];
+  0x01, 0x00, 0x00, 0x01, 0x32,
+  0x01, 0x00, 0x00, 0x01, 0x32,
+  0x01, 0x00, 0x00, 0x01, 0x32,
+  0x01, 0x00, 0x00, 0x01, 0x32,
+  0x01, 0x00, 0x00, 0x01, 0x32];
 
 for (var i = 0, len = bytes.length; i < len; ++i) {
-  parser.write(new Buffer([bytes[i]]));
+  decoder.write(new Buffer([bytes[i]]));
 }
 
 for (var i = 0; i < 5; ++i) {
-  msgs[i].meta.length.should.equal(1);
-  msgs[i].meta.format.should.equal(0);
-  msgs[i].payload.toString().should.equal('2');
+  msgs[i].body.toString().should.equal('2');
 }
 
-// test pack()
+// test well formed multipart
 
 msgs = [];
 
-parser.write(parser.pack('hello', 0));
-parser.write(parser.pack('how are you doing?', 0));
-parser.write(parser.pack('that is good tobo', 1));
+decoder.write(new Buffer([
+  0x00, 0x00, 0x00, 0x12,
+  0x01, 0x00, 0x00, 0x05, 0x68, 0x65, 0x6c, 0x6c, 0x6f,
+  0x01, 0x00, 0x00, 0x05, 0x77, 0x6f, 0x72, 0x6c, 0x64
+]));
 
-msgs[0].payload.toString().should.equal('hello');
-msgs[1].payload.toString().should.equal('how are you doing?');
-msgs[2].payload.toString().should.equal('that is good tobo');
+msgs[0].body.should.be.instanceof(Array);
+msgs[0].body.should.have.length(2);
+msgs[0].body[0].toString().should.equal('hello');
+msgs[0].body[1].toString().should.equal('world');
 
-msgs[0].meta.length.should.equal(5);
-msgs[1].meta.length.should.equal(18);
-msgs[2].meta.length.should.equal(17);
 
-msgs[0].meta.format.should.equal(0);
-msgs[1].meta.format.should.equal(0);
-msgs[2].meta.format.should.equal(1);
+// test single byte multipart
+
+msgs = [];
+
+var bytes = [
+  0x00, 0x00, 0x00, 0x12,
+  0x01, 0x00, 0x00, 0x05, 0x68, 0x65, 0x6c, 0x6c, 0x6f,
+  0x01, 0x00, 0x00, 0x05, 0x77, 0x6f, 0x72, 0x6c, 0x64
+];
+
+for (var i = 0, len = bytes.length; i < len; ++i) {
+  decoder.write(new Buffer([bytes[i]]));
+}
+
+msgs[0].body.should.be.instanceof(Array);
+msgs[0].body.should.have.length(2);
+msgs[0].body[0].toString().should.equal('hello');
+msgs[0].body[1].toString().should.equal('world');
+
+
+
+
+
+
+
+
+
+
 
