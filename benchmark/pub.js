1e9a6bc08ee74c9e3a1c5598f8b3898a2fe85eb4
TJ Holowaychuk <tj@vision-media.ca>
2012-08-03 20:21:38 -0700

    add --per-tick and --size to benchmark/pub

--- benchmark/pub.js
+++ benchmark/pub.js
@@ -1,12 +1,18 @@
 
 var ss = require('..')
-  , sock = ss.socket('pub');
+  , sock = ss.socket('pub')
+  , program = require('commander');
+
+program
+  .option('-t, --per-tick <n>', 'messages per tick [1000]', parseInt)
+  .option('-s, --size <n>', 'message sizein bytes [1024]', parseInt)
+  .parse(process.argv)
 
 sock.bind(3000);
 console.log('pub bound');
 
-var perTick = 1000;
-var buf = new Buffer(Array(1024).join('a'));
+var perTick = program.perTick || 1000;
+var buf = new Buffer(Array(program.size || 1024).join('a'));
 console.log('sending %d per tick', perTick);
 console.log('sending %d byte messages', buf.length);
 
