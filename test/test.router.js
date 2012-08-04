d3b8c80703eff9e9bdd143b0503ed60e0045be2b
Garrett Johnson <gjohnson@redventures.com>
2012-08-04 17:17:34 -0400

    First stab at Router.

---
+++ test/test.router.js
@@ -0,0 +1,22 @@
+
+var ss = require('../')
+  , should = require('should');
+
+var router = ss.socket('router')
+  , worker = ss.socket('stream');
+
+worker.option('identity', 'myworker');
+worker.connect(3000);
+
+worker.on('message', function(msg){
+  msg.toString().should.equal('hey there tobi');
+  worker.close();
+  router.close();
+});
+
+router.option('identity', 'myrouter');
+router.bind(3000);
+
+setTimeout(function(){
+  router.send('myworker', 'hey there tobi');
+}, 250);
