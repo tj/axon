5a8a56b53402b932113e908db36e9f0326d66f09
TJ Holowaychuk <tj@vision-media.ca>
2012-10-10 17:41:25 -0700

    add emitter many .connect() test

--- test/test.emitter.many-connect.js
+++ test/test.emitter.many-connect.js
@@ -3,30 +3,24 @@ var ss = require('..')
   , should = require('should');
 
 var worker = ss.socket('pub-emitter')
-  , relaySub = ss.socket('sub-emitter')
-  , relayPub = ss.socket('pub-emitter')
   , a = ss.socket('sub-emitter')
   , b = ss.socket('sub-emitter')
   , c = ss.socket('sub-emitter')
 
 /*
 
-                    +--> a
-  worker ---> relay |--> b 
-                    +--> c
+            +--> a
+  worker ---|--> b 
+            +--> c
 
 */
 
-relaySub.bind(3000);
-relayPub.bind(4000);
 worker.connect(3000);
-a.connect(4000);
-b.connect(4000);
-c.connect(4000);
-
-relaySub.on('progress', function(id, n){
-  relayPub.emit('progress', id, n);
-});
+worker.connect(3001);
+worker.connect(3002);
+a.bind(3000);
+b.bind(3001);
+c.bind(3002);
 
 var vals = [];
 var pending = 3;
@@ -62,8 +56,6 @@ function done() {
   vals.should.include('c');
   vals.should.have.length(3);
   worker.close();
-  relaySub.close();
-  relayPub.close();
   a.close();
   b.close();
   c.close();
