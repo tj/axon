ad2e353f0e13baab9cd5ccbe5ed49513992de730
EJ Bensing <ebensing@rm-dash-r.com>
2013-09-11 20:28:51 -0700

    added test & fixed bugs

---
+++ test/test.reqrep.priority.js
@@ -0,0 +1,52 @@
+
+var axon = require('..')
+  , should = require('should');
+
+var req = axon.socket('req')
+  , rep1 = axon.socket('rep')
+  , rep2 = axon.socket('rep');
+
+var count1 = 0;
+var count2 = 0;
+
+var msgCount = 10;
+var seen = 0;
+
+req.bind(4000);
+rep1.connect(4000);
+rep2.connect(4000);
+
+req.usePriority(true);
+
+rep1.on('message', function(msg, reply){
+  setTimeout(function () {
+    count1++;
+    reply('got "' + msg + '"');
+  }, 1200);
+});
+
+rep2.on('message', function(msg, reply){
+  setTimeout(function () {
+    count2++;
+    reply('got "' + msg + '"');
+  }, 9000);
+});
+
+var interId = setInterval(function () {
+  req.send('hello', function(msg){
+    msg.toString().should.equal('got "hello"');
+    seen++;
+    if (seen == msgCount) {
+      clearInterval(interId);
+      done();
+    }
+  });
+}, 2000);
+
+function done() {
+  count1.should.equal(8);
+  count2.should.equal(2);
+  rep1.close();
+  rep2.close();
+  req.close();
+}
