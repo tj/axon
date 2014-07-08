04efb1d9cb6f412a1dddf6c15799a3ed330f25c9
Garrett Johnson <garrett@recurly.com>
2014-07-08 01:20:45 -0400

    Gracefully close sockets

--- test/test.socket.close.js
+++ test/test.socket.close.js
@@ -3,17 +3,29 @@ var axon = require('..')
   , assert = require('better-assert');
 
 var pull = axon.socket('pull');
-
+var push = axon.socket('push');
 var closed = false;
+var msgs = [];
 
 pull.bind(4000, function(){
-  pull.close(function(){
+  push.connect(4000);
+});
+
+push.on('connect', function(){
+  push.send('a');
+  push.send('b');
+  push.send('c');
+  push.close(function(){
     closed = true;
+    assert('abc' == msgs.join(''));
+    pull.close();
   });
 });
 
+pull.on('message', function(msg){
+  msgs.push(msg);
+});
+
 pull.on('close', function(){
-  setTimeout(function(){
-    assert(closed);
-  }, 100);
+  assert(closed);
 });
