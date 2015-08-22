1af655b3b0cafd2d1e3a95d5152c5ad5dd935044
Eugen Tudorancea <eugen.tudorancea@gmail.com>
2015-08-22 14:18:47 +0300

    Fix socket reconnect when using unix sockets (unix://)

---
+++ test/test.socket.reconnect.unix.js
@@ -0,0 +1,45 @@
+
+var ss = require('../')
+  , should = require('should')
+  , assert = require('assert');
+
+var push = ss.socket('push')
+  , pull = ss.socket('pull');
+
+// basic 1-1 push/pull
+
+var msgs = []
+  , n = 0
+  , address = 'unix:///tmp/test.unix.sock';
+
+push.bind(address);
+pull.connect(address);
+
+var id = setInterval(function(){
+  push.send(String(n++));
+}, 5);
+
+pull.on('message', function(msg){
+  msgs.push(msg.toString());
+
+  switch (msgs.length) {
+    case 10:
+      pull.close();
+      pull.once('close', function(){
+        setTimeout(function(){
+          pull.connect(address);
+        }, 50);
+      });
+      break;
+    case 300:
+      for (var i = 0; i < 299; ++i) {
+        msgs[i].should.equal(i.toString());
+      }
+
+      clearInterval(id);
+
+      push.close();
+      pull.close();
+      break;
+  }
+});
